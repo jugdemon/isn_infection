528510ac1e82c68386b871537fe1e2f9df4f5b90
Jascha Grübel <jgruebel@student.ethz.ch>
2017-03-10 13:22:15 +0100

    Add comparison of infections

--- R/infection.R
+++ R/infection.R
@@ -1,3 +1,7 @@
+devtools::use_package('GGally')
+devtools::use_package('ggplot2')
+devtools::use_package('RColorBrewer')
+
 #' Infect Network
 #'
 #' A function that infects a random individual of a network and
@@ -37,4 +41,55 @@ infect_network <- function(network, repetitions, network.size,random.seed = NULL
   }
   
   return(diseasedIndex)
+}
+
+#' Compare Infections
+#'
+#' A function that takes takes two copies of a network,
+#' one vaccinated and one unvaccinated
+#' 
+#' @param unvac.network Network with no vaccinations
+#' @param vac.network Network with vaccinations
+#' @param intervention Vector of nodes representing vaccinated people
+#' @param infection.steps Number of steps that the infection will spread.
+#' @keywords Infection, Network, Comparison
+#' @export
+#' @return List with the graph representation of the unvaccinated network [[1]],
+#' the vaccinated network [[2]], the infected index under no vaccination [[3]],
+#' and the infected index under vaccination [[4]].
+#' @examples 
+#' n=100
+#' mat <- rgraph(n, mode = "graph", tprob = 0.5)
+#' net <- network(mat, directed = FALSE)
+#' results <- identify_vaccination_targets(igraph::betweenness,mat,n,5)
+#' intervention <- results[[1]]
+#' vaccinatedMat <- do.call(rbind,results[2])
+#' vacNet <- network(vaccinatedMat, directed = FALSE)
+#' results2 <- compare_infections(net,vacNet,intervention,3)
+#' 
+compare_infections <- function(unvac.network,vac.network,intervention,infection.steps){
+  
+  infected.index <- isnInfection::infect_network(unvac.network, infection.steps, n,seed)
+  unvac.network %v% "States (1)" = infected.index
+  unvacNetG<-GGally::ggnet2(unvac.network,size = 0, mode = c("x", "y"),
+                    color="States (1)",
+                    palette = "Set2",
+                    legend.size = 26)+
+    ggplot2::geom_point(ggplot2::aes(color = color), size = 10, color = "black", alpha = 0.5) +
+    ggplot2::geom_point(ggplot2::aes(color = color), size = 9) +
+    ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size=12)))
+  
+  
+  infected.vac.index <- isnInfection::infect_network(vac.network, infection.steps, n,seed)
+  infected.vac.index <- ifelse(intervention=="Vaccinated",intervention, infected.vac.index)
+  unvac.network %v% "States (1)" = infected.vac.index
+  vacNetG <- GGally::ggnet2(unvac.network,size = 0, mode = c("x", "y"),
+                    color="States (1)",
+                    palette = "Set2",
+                    legend.size = 26)+
+    ggplot2::geom_point(ggplot2::aes(color = color), size = 10, color = "black", alpha = 0.5) +
+    ggplot2::geom_point(ggplot2::aes(color = color), size = 9) +
+    ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size=12)))
+  
+  return(list(unvacNetG,vacNetG,infected.index,infected.vac.index))
 }
--- R/vaccination.R
+++ R/vaccination.R
@@ -13,45 +13,17 @@ devtools::use_package('igraph')
 #' mat = matrix(rbinom(10 * 5, 1, 0.3), ncol = 10, nrow = 10)
 #' vaccinatedIndex <- identify_vaccination_targets(igraph::betweenness,mat)
 #' 
-identify_vaccination_targets <- function(centrality_measure,data,network.size){
+identify_vaccination_targets <- function(centrality_measure,data,network.size,treatments){
+  
   g <- igraph::as.undirected(igraph::graph.adjacency(data),"collapse")
-  index1 = which.max(centrality_measure(g))
-  needle.sharing1 <- data
-  needle.sharing1[index1,]=0
-  needle.sharing1[,index1]=0
-  g1 <- igraph::as.undirected(igraph::graph.adjacency(needle.sharing1),"collapse")
+  intervention <- rep(x="Untreated",network.size)
+  for (i in 1:treatments){
+    index = which.max(centrality_measure(g))
+    intervention[index]="Vaccinated"
+    data[index,]=0
+    data[,index]=0
+    g <- igraph::as.undirected(igraph::graph.adjacency(data),"collapse")
+  }
   
-  index2 = which.max(centrality_measure(g1))
-  needle.sharing2 <- needle.sharing1
-  needle.sharing2[index2,]=0
-  needle.sharing2[,index2]=0
-  g2 <- as.undirected(graph.adjacency(needle.sharing2),"collapse")
-  
-  index3 = which.max(igraph::betweenness(g2))
-  needle.sharing3 <- needle.sharing2
-  needle.sharing3[index3,]=0
-  needle.sharing3[,index3]=0
-  g3 <- as.undirected(graph.adjacency(needle.sharing3),"collapse")
-  
-  index4 = which.max(centrality_measure(g3))
-  needle.sharing4 <- needle.sharing3
-  needle.sharing4[index4,]=0
-  needle.sharing4[,index4]=0
-  g4 <- as.undirected(graph.adjacency(needle.sharing4),"collapse")
-  
-  index5 = which.max(centrality_measure(g4))
-  needle.sharing5 <- needle.sharing4
-  needle.sharing5[index5,]=0
-  needle.sharing5[,index5]=0
-  g5 <- as.undirected(graph.adjacency(needle.sharing5),"collapse")
-  
-  intervention <- rep(x=0,network.size)
-  intervention[index1]=1
-  intervention[index2]=1
-  intervention[index3]=1
-  intervention[index4]=1
-  intervention[index5]=1
-  intervention <- ifelse(intervention==1,"Vaccinated","Untreated")
-  
-  return(list(intervention,needle.sharing5))
+  return(list(intervention,data))
 }
