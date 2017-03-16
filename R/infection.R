1e1296b4a3aada9320e2f38692dbb598375645a8
Jascha Grübel <jgruebel@student.ethz.ch>
2017-03-16 17:57:15 +0100

    Complete R package

--- R/infection.R
+++ R/infection.R
@@ -67,12 +67,13 @@ infect_network <- function(network, repetitions, network.size,random.seed = NULL
 #' vacNet <- network(vaccinatedMat, directed = FALSE)
 #' results2 <- compare_infections(net,vacNet,intervention,3)
 #' 
-compare_infections <- function(unvac.network,vac.network,intervention,infection.steps){
+compare_infections <- function(unvac.network,vac.network,intervention,infection.steps,random.seed){
   
-  infected.index <- isnInfection::infect_network(unvac.network, infection.steps, n,seed)
-  unvac.network %v% "States (1)" = infected.index
+  infected.index <- isnInfection::infect_network(unvac.network, infection.steps, n,random.seed)
+  name <- paste("States (",paste(infection.steps,")", sep = ""), sep = "")
+  unvac.network %v% name = infected.index
   unvacNetG<-GGally::ggnet2(unvac.network,size = 0, mode = c("x", "y"),
-                    color="States (1)",
+                    color=name,
                     palette = "Set2",
                     legend.size = 26)+
     ggplot2::geom_point(ggplot2::aes(color = color), size = 10, color = "black", alpha = 0.5) +
@@ -80,11 +81,11 @@ compare_infections <- function(unvac.network,vac.network,intervention,infection.
     ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size=12)))
   
   
-  infected.vac.index <- isnInfection::infect_network(vac.network, infection.steps, n,seed)
+  infected.vac.index <- isnInfection::infect_network(vac.network, infection.steps, n,random.seed)
   infected.vac.index <- ifelse(intervention=="Vaccinated",intervention, infected.vac.index)
-  unvac.network %v% "States (1)" = infected.vac.index
+  unvac.network %v% name = infected.vac.index
   vacNetG <- GGally::ggnet2(unvac.network,size = 0, mode = c("x", "y"),
-                    color="States (1)",
+                    color=name,
                     palette = "Set2",
                     legend.size = 26)+
     ggplot2::geom_point(ggplot2::aes(color = color), size = 10, color = "black", alpha = 0.5) +
@@ -92,4 +93,48 @@ compare_infections <- function(unvac.network,vac.network,intervention,infection.
     ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size=12)))
   
   return(list(unvacNetG,vacNetG,infected.index,infected.vac.index))
+}
+
+#' Test random Infections
+#'
+#' A function that takes takes two copies of a network,
+#' one vaccinated and one unvaccinated and test random
+#' infection in it.
+#' 
+#' For each random sample the infection will be simulated
+#' for a the specified amount of steps.
+#' 
+#' @param unvac.network Network with no vaccinations
+#' @param vac.network Network with vaccinations
+#' @param intervention Vector of nodes representing vaccinated people
+#' @param network.size Number of nodes in the network.
+#' @param infection.steps Number of steps that the infection will spread.
+#' @param random.samples Vector of nodes that will be infected.
+#' @keywords Infection, Network, Comparison
+#' @export
+#' @return List with the number of infection in the unvaccinated network [[1]]
+#' and the vaccinated network [[2]].
+#' @examples 
+#' n=100
+#' mat <- rgraph(n, mode = "graph", tprob = 0.5)
+#' net <- network(mat, directed = FALSE)
+#' results <- identify_vaccination_targets(igraph::betweenness,mat,n,5)
+#' intervention <- results[[1]]
+#' vaccinatedMat <- do.call(rbind,results[2])
+#' vacNet <- network(vaccinatedMat, directed = FALSE)
+#' seed <- sample.int(n,10,replace = TRUE)
+#' results2 <- test_random_infections(net,vacNet,intervention,n,3,seed)
+#' 
+test_random_infections <- function(unvac.network,vac.network,intervention,network.size,infection.steps,random.samples){
+  infection.count <- rep(0,network.size)
+  infection.vac.count <- rep(0,network.size)
+  i <- 0
+  for (sample in random.samples){
+    results <- compare_infections(unvac.network,vac.network,intervention,infection.steps,sample)
+    infection.count<- ifelse (results[[3]]=="Infected",infection.count+1,infection.count)
+    infection.vac.count<- ifelse (results[[4]]=="Infected" & intervention=="Untreated",infection.vac.count+1,infection.vac.count)
+    i <- i+1
+    print(i)
+  }
+  return(list(infection.count,infection.vac.count))
 }
--- R/vaccination.R
+++ R/vaccination.R
@@ -16,7 +16,8 @@ devtools::use_package('igraph')
 identify_vaccination_targets <- function(centrality_measure,data,network.size,treatments){
   
   g <- igraph::as.undirected(igraph::graph.adjacency(data),"collapse")
-  intervention <- rep(x="Untreated",network.size)
+  intervention <- rep(x=0,network.size)
+  intervention <- ifelse(intervention==0,"Untreated","Untreated")
   for (i in 1:treatments){
     index = which.max(centrality_measure(g))
     intervention[index]="Vaccinated"
@@ -26,4 +27,35 @@ identify_vaccination_targets <- function(centrality_measure,data,network.size,tr
   }
   
   return(list(intervention,data))
+}
+
+#' Apply Vaccination Patterns
+#'
+#' A function that applies different vaccination patterns to a 
+#' network.
+#' 
+#' @param centrality_measures alist of igraph centrality measure
+#' @param data An adjacency matrix, possibly directed
+#' @param data.size Number of columns in the matrix
+#' @param treatments Number of treatments available
+#' @keywords Network, Centrality, COmparison
+#' @export
+#' @return List of Lists with all the vaccination vectors [[1]] and
+#' the vaccinated matrices [[2]] in the order of the centrality_measures
+#' list.
+#' @examples 
+#' mat = matrix(rbinom(10 * 5, 1, 0.3), ncol = 10, nrow = 10)
+#' vaccinatedIndex <- apply_vaccination_patterns(list(igraph::betweenness,igraph::closeness),mat,10,2)
+#' 
+apply_vaccination_patterns <- function(centrality_measures,data,data.size,treatments){
+  interventions = list()
+  matrices = list()
+  i <- 1
+  for (measure in centrality_measures){
+    results <- identify_vaccination_targets(measure,data,data.size,treatments)
+    interventions[[i]] <- results[[1]]
+    matrices[[i]] <- results[[2]]
+    i <- i + 1
+  }
+  return(list(interventions,matrices))
 }
